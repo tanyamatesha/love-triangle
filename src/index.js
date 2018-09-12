64df6514554765c5e3af44a37bf8bb6d1dec8fe1
tanyamatesha <tanya-matesha@rambler.ru>
2018-09-12 22:20:00 +0300

    love-triangle is complited.

--- src/index.js
+++ src/index.js
@@ -3,5 +3,16 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    let count = 0;
+    preferences.forEach((value, index) => {
+        if (value !== 0) {
+            let val = value - 1;
+            if (index !== val)
+                if (index === preferences[preferences[val] - 1] - 1) {
+                    count++;
+                    preferences[index] = preferences[val] = preferences[preferences[val] - 1] = 0;
+                }
+        }
+    });
+    return count;
 };
